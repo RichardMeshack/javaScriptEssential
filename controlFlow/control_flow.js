c86b2d162a56dc2041dfdf21f00bcf2a10a4165e
richard meshack rotji <rotjirichard1@gmail.com>
2025-03-27 10:11:51 +0100

    Added control flow

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -50,3 +50,7 @@ switch(userType){
 }
 
 console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
